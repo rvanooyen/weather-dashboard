3e9e2a2df95ae4c4b327ad6233bf72fbe0409cb1
rvanooyen <robert.vanooyen@gmail.com>
2021-09-14 05:43:23 -0500

    update 3 to in progress API Call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,17 @@
-// var createTable = function() {
-//     for (i = 0; i < hoursArr.length; i++) {       
-//         var rowEl = document.createElement("div");
-//         var labelEl = document.createElement("div");
-//         var colEl = document.createElement("div");
-//         var buttonEl = document.createElement("button");
-//         rowEl.setAttribute("id", "rowHeading");
-//         rowEl.setAttribute("class", "row");
-//         labelEl.setAttribute("id", "labelTitle");
-//         labelEl.setAttribute("class", "col-sm-2 hour");
-//         labelEl.setAttribute("p", hoursArr[i]);
-//         colEl.setAttribute("id", "colTitle");
-//         var textInputEl = document.createElement("textarea");
-//         textInputEl.setAttribute("style", "dispaly:none");
-//         colEl.append(textInputEl);        
-
-//         // apply new class if task is past present or future
-//         if (moment().hour() > hoursArr[i]) {
-//             colEl.setAttribute("class", "col-sm-8 past");
-//         } else if (moment().hour() === hoursArr[i]) {
-//             colEl.setAttribute("class", "col-sm-8 present");
-//         } else {
-//             colEl.setAttribute("class", "col-sm-8 future");
-//         }
-//         if (hoursArr[i] < 12) {
-//             labelEl.textContent = moment().hour(hoursArr[i]).format("h") + ":00 AM";
-//         } else {
-//             labelEl.textContent = moment().hour(hoursArr[i]).format("h") + ":00 PM";
-//         }               
-//         buttonEl.setAttribute("id", "saveButton");
-//         buttonEl.setAttribute("class", "btn col-sm-2");
-//         // buttonEl.setAttribute();
-//         rowEl.appendChild(labelEl);
-//         rowEl.appendChild(colEl);
-//         rowEl.appendChild(buttonEl);        
-//         $("#hours-container").append(rowEl);        
-//     }
-// };
-
+// Weather Dashboard
 // user clicks on search button
+// saves a copy of the search location 
+// displays the current weather and five day forecast for the location
+
 $("#search-btn").on("click", function(event) {           
     // get values from text fields
     var city = document.getElementById("city-input").value;    
-    var data = getWeather(city);    
+    getWeather(city);    
 });
 
 var getWeather = function(city) {
     var apiKey = "&appid=" + "f4574024d0e4cb4ce006c01517d66eb9";
-    var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q="
-    // var urlOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + apiKey
+    var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q="    
     
     fetch(urlWeather + city + apiKey)
         .then(function(response) {
@@ -56,40 +20,49 @@ var getWeather = function(city) {
         .then(function(response) {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
-            // getOneCall(lat, lon);
-            // fetch(urlOneCall(lat, lon)
-            // .then(function(response) {
-            //     return response.json();                
-            // })
-            // .then(function(response) {
-                console.log(response);
-                console.log(response.main.humidity);
-                console.log(response.main.temp);
-                console.log(response.name); 
-                console.log(response.wind.speed);
-            // })           
-        });
-
-    
-    //     var responseContainerEl = document.querySelector('#response-container');
-    //     responseContainerEl.innerHTML = '';
-    //     var gifImg = document.createElement('img');
-    //     gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
-    //     responseContainerEl.appendChild(gifImg);
-    //   });
-  // }    
-};
-
-var getOneCall = function(lat, lon) {
-    fetch(urlOneCall)
+            
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&units=imperial" + apiKey)
             .then(function(response) {
                 return response.json();                
             })
             .then(function(response) {
-                console.log(response);
-                console.log(response.main.humidity);
-                console.log(response.main.temp);
-                console.log(response.name); 
-                console.log(response.wind.speed);
-    });
+                console.log(response);               
+
+                // creates forecast elements
+                var cityEl = document.createElement("p");
+                var tempEl = document.createElement("p");
+                var windEl = document.createElement("p");
+                var humidityEl = document.createElement("p");
+                var uvIndexEl = document.createElement("P");
+                var iconEl = document.createElement("i");
+
+                // sets forecast elements
+                cityEl.setAttribute("id", "city-element");
+                cityEl.setAttribute("class", "city-element");
+                cityEl.textContent = city + "(" + moment().format("l") + ")" + response.current.weather[0].icon + ".png";                
+                iconEl.setAttribute("class", response.current.weather[0]);                                
+                tempEl.setAttribute("id", "city-temp");
+                tempEl.textContent = "Temp: " + response.current.temp + " F";
+                windEl.setAttribute("id", "city-wind");
+                windEl.textContent = "Wind: " + response.current.wind_speed + " MPH";
+                humidityEl.setAttribute("id", "city-humiditiy");
+                humidityEl.textContent = "Humidity: " + response.current.humidity + "%";
+                uvIndexEl.setAttribute("id", "city-uvIndex");
+                uvIndexEl.setAttribute("class", "uv-element");
+                uvIndexEl.textContent = "UV Index: " + response.current.uvi;
+
+                // appends current weather to dashboard
+                $("#city-element").append(iconEl);
+                $("#today-forecast").append(cityEl);                
+                $("#today-forecast").append(tempEl);
+                $("#today-forecast").append(windEl);
+                $("#today-forecast").append(humidityEl);
+                $("#today-forecast").append(uvIndexEl);
+
+                // appends five day forecast to dashboard
+                for (i = 0; i < 5; i++) {
+
+                };                
+            })           
+        });     
 };
